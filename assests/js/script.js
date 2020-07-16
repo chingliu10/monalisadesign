17bb3c55ec3549c043c759a7b3de0ff5e54aa3e2
chingliu10 <56655368+chingliu10@users.noreply.github.com>
2020-07-16 17:17:44 +0300

    finishing the transition side trying to modify it

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,26 +1,42 @@
 
-let menu = document.querySelector(".humberMenu");
+// let menu = document.querySelector(".humberMenu");
 
-let menuVisibility = false;
+// let menuVisibility = false;
 
-menu.addEventListener("click", triggerNavigation);
+// menu.addEventListener("click", triggerNavigation);
 
-function triggerNavigation(){
+// function triggerNavigation(){
 
-    let getMenu = document.querySelector(".bottomNav");
+//     let getMenu = document.querySelector(".bottomNav");
 
-    if(menuVisibility === false){
+//     if(menuVisibility === false){
     
-    getMenu.style.top = "10vh";
-    menuVisibility = true;
+//     getMenu.style.top = "10vh";
+//     menuVisibility = true;
 
-    }else{
+//     }else{
     
-    getMenu.style.top = "-80vh";
-    menuVisibility = false;
+//     getMenu.style.top = "-80vh";
+//     menuVisibility = false;
 
-    }
+//     }
 
-}
+// }
 
 /*the above code triggers the menu*/
+
+//animation code for collection
+
+let collection = document.querySelector(".collectionWrapper");
+console.log("this offsetTop from collection " + collection.offsetTop);
+
+window.addEventListener("scroll", function(){
+    
+    if(this.pageYOffset >= 250){
+        collection.style.marginTop = "8vh";
+    }
+});
+
+function performAnimation(){
+
+}
