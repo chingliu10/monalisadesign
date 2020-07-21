30fd678a3ba1e6d4e36569d469fe8b9690473b5f
chingliu10 <56655368+chingliu10@users.noreply.github.com>
2020-07-21 17:25:44 +0300

    node intial setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const hbs = require("hbs");
+const express = require("express");
+
+let app = express();
+let port = process.env.PORT || 3000;
+
+app.use(express.static("assests"));
+app.set("view engine", "hbs");
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+
+
+app.listen(port, () => {
+    console.log(`listenning tot port ${port}`);
+})
