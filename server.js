8e0ae6fe6d207fe246e8a8582a710a14c1a6b242
chingliu10 <56655368+chingliu10@users.noreply.github.com>
2020-07-22 03:26:39 +0300

    reformatting the bottom navigation

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const hbs = require("hbs");
 const express = require("express");
 
 let app = express();
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 8000;
 
 app.use(express.static("assests"));
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
 
 app.get("/", (req, res) => {
